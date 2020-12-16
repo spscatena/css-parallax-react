c2256e7600e7359caca18bb0b302bfd76c72fa80
Sabrina Scatena <sabrina.scatena@gmail.com>
2020-12-16 00:43:03 -0500

    css parallax react

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function NavBar() {
+  return (
+    <header>
+      <nav>
+        <div id="header-home">
+          <a href="">Home</a>
+        </div>
+        <div id="header-rt-side">
+          <a href="">About</a>
+          <a href="">Portfolio</a>
+          <a href="">Contact</a>
+        </div>
+      </nav>
+      <p id="header-p">Web Developer</p>
+    </header>
+  )
+}
